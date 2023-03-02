d8e858f5fa3e6ee1aa770c29368a9182a5b6e995
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-02 07:07:50 +0330

    Function: needsLicense

--- vehicle-purchase.js
+++ vehicle-purchase.js
@@ -1,1 +1,7 @@
 // this comment is for me ---> look back again this 😶😑😐
+
+function needsLicense(kind) {
+  return kind === "car" || kind === "truck";
+}
+
+// console.log(needsLicense("car"), needsLicense("bike"));
